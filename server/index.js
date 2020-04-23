84b111ce51fd8f8ea20f655f986f0525c1de7c0b
Christine Ting <christ123092@yahoo.com>
2020-04-23 15:55:46 -0700

    completed code

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const router = require('./router');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
 
